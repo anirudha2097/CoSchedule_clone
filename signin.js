2395689d3eb2e8d4289f8970160523821561ee53
Suneet25 <suneetpanigrahi2505@gmail.com>
2022-08-24 22:40:09 +0530

    added project day1

---
+++ signin.js
@@ -0,0 +1,25 @@
+let signupData = JSON.parse(localStorage.getItem("signup")) || [];
+  let signin = () => {
+    let signinData = {
+      email: document.getElementById("email").value,
+      password: document.getElementById("password").value,
+    };
+let query=false;
+    signupData.forEach((el) => {
+      if (
+        el.email === signinData.email &&
+        el.password === signinData.password
+      ) {
+        query=true;
+      }
+     
+    });
+    if(query)
+    {
+      alert("Signin successful");
+    }
+    else
+    {
+      alert("User doesn't exist");
+    }
+  };
